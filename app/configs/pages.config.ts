1d279e529b5bb6311d51ebe48c158637fc5fd382
Ekaterina-timans <128321173+Ekaterina-timans@users.noreply.github.com>
2024-07-19 15:01:39 +0300

    Create a project

---
+++ app/configs/pages.config.ts
@@ -0,0 +1,11 @@
+class DASHBOARD {
+	private root = '/'
+	STATISTICS = this.root
+	COMMON_TASKS = `${this.root}/common-tasks`
+	PERSONAL_TASKS = `${this.root}/personal-tasks`
+	CHAT = `${this.root}/chat`
+    SETTINGS = `${this.root}/settings`
+    ADMIN_PANEL = `${this.root}/admin-panel`
+}
+
+export const DASHBOARD_PAGES = new DASHBOARD()
